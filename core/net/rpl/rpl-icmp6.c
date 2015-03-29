b943532f2e9bca1dde183bbb9db200c9584a4a59
Csaba Kiraly <kiraly@disi.unitn.it>
2015-03-29 22:40:43 +0200

    DAO: record DAO sequence number in route entry when DAO is sent
    DAO sequence numbers should be recorded so that later DAO ACKs
    could be matched with the right route entry.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- core/net/rpl/rpl-icmp6.c
+++ core/net/rpl/rpl-icmp6.c
@@ -87,6 +87,7 @@ void RPL_DEBUG_DAO_OUTPUT(rpl_parent_t *);
 #endif
 
 static uint8_t dao_sequence = RPL_LOLLIPOP_INIT;
+static uint8_t myaddr_dao_sequence;
 
 extern rpl_of_t RPL_OF;
 
@@ -590,7 +591,7 @@ dao_input(void)
   uint8_t pathsequence;
   */
   uip_ipaddr_t prefix;
-  uip_ds6_route_t *rep;
+  uip_ds6_route_t *rep = NULL;
   uint8_t buffer_length;
   int pos;
   int len;
@@ -814,6 +815,7 @@ dao_output(rpl_parent_t *parent, uint8_t lifetime)
 
   /* Sending a DAO with own prefix as target */
   dao_output_target(parent, &prefix, lifetime);
+  myaddr_dao_sequence = dao_sequence;
 }
 /*---------------------------------------------------------------------------*/
 void
@@ -902,6 +904,10 @@ dao_output_target(rpl_parent_t *parent, uip_ipaddr_t *prefix, uint8_t lifetime)
   PRINTF("\n");
 
   if(rpl_get_parent_ipaddr(parent) != NULL) {
+    uip_ds6_route_t *rep;
+    if ((rep = uip_ds6_route_lookup(prefix))) {
+      rep->state.dao_sequence = dao_sequence;
+    }
     uip_icmp6_send(rpl_get_parent_ipaddr(parent), ICMP6_RPL, RPL_CODE_DAO, pos);
   }
 }
@@ -920,6 +926,9 @@ dao_forward(const uip_ipaddr_t *dest, int type, int code, int payload_len, uip_d
   RPL_LOLLIPOP_INCREMENT(dao_sequence);
   UIP_ICMP_PAYLOAD[3] = dao_sequence;
 
+  if (rep) {
+    rep->state.dao_sequence = dao_sequence;
+  }
   uip_icmp6_send(dest, type, code, payload_len);
 }
 /*---------------------------------------------------------------------------*/
