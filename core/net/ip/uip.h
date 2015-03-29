427ac57338d4953697d655c20ce40c62f7221319
Csaba Kiraly <kiraly@disi.unitn.it>
2015-03-29 21:48:19 +0200

    mcaster: define extension header strucutre

--- core/net/ip/uip.h
+++ core/net/ip/uip.h
@@ -1792,6 +1792,15 @@ typedef struct uip_routing_hdr {
   uint8_t seg_left;
 } uip_routing_hdr;
 
+/* MCASTER option */
+typedef struct uip_mcaster_hdr {
+  uint8_t next;
+  uint8_t len;
+  uint16_t align1;
+  uint32_t align2;
+  uip_ip6addr_t destipaddr;
+} uip_mcaster_hdr;
+
 /* fragmentation header */
 typedef struct uip_frag_hdr {
   uint8_t next;
